9c47511f4b1a28f57345850f748c0603b281ec7c
Masashi Sato <ms2.sato@gmail.com>
2023-04-20 14:52:03 +0900

    add log

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { appMention } from './mention';
 (async () => {
   // アプリを起動します
   await app.start(process.env.PORT || 3000)
+  console.log('⚡️ Bolt app is running!', process.env.PORT);
 })()
 app.shortcut({ callback_id: 'query', type: 'message_action' }, shortcutQuery(app))
 app.event('app_mention', appMention)
