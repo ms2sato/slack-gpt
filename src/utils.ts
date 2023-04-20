c6fd6d8f3011574b29abac8ae793358293847207
Masashi Sato <ms2.sato@gmail.com>
2023-04-20 15:12:59 +0900

    change default model

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration)
 const MAX_TOKENS = 8192
 async function getNumberOfTokens (messages: ChatCompletionRequestMessage[]): Promise<number> {
   let length = 0
-  const encoding = encoding_for_model('gpt-4')
+  const encoding = encoding_for_model('text-davinci-003')
   for (const message of messages) {
     if (message.role === ChatCompletionRequestMessageRoleEnum.User) {
       length += encoding.encode(message.content).length
@@ -18,7 +18,7 @@ async function getNumberOfTokens (messages: ChatCompletionRequestMessage[]): Pro
   return length
 }
 
-export async function ask (messages: ChatCompletionRequestMessage[], model = 'gpt-4') {
+export async function ask (messages: ChatCompletionRequestMessage[], model = 'text-davinci-003') {
   const response = await openai.createChatCompletion({
     model,
     messages
